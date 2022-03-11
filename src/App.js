bde552df1ec72df041c9d9d091103a1ca5747274
Austin Zu <austinzuffi@gmail.com>
2022-03-10 20:05:16 -0800

    trippy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,30 @@ function App() {
 
   return (
     <div className="App">
-	  <Carousel views={images} />;
+      <header className="App-header">
+	<h1>Surely Magazine</h1>
+      </header>
+      <div>
+      <Carousel views={images} />;
+      </div>
+      <footer className="App-header">
+	<a
+          className="App-link"
+          href="https://www.instagram.com/surely_magazine/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          instagram
+        </a>
+	<a
+          className="App-link"
+	  href="https://www.dropbox.com/s/m8fx4t0l5hac4xa/Paolo%20%40BGC.WAV"
+          target="_blank"
+          rel="noopener noreferrer"
+	>
+	  listen
+	</a>
+      </footer>
     </div>
   );
 }
