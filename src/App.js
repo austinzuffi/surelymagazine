abdf230f37bffc393d52c62c09f7bd726b053f18
Austin Zu <austinzuffi@gmail.com>
2022-03-10 19:48:31 -0800

    whoaw

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import surelymagazine from './surelymagazine.png';
 import './App.css';
+import Carousel from 'react-images';
+
 
 function App() {
+
+
+  const images = [
+    {
+      source: surelymagazine
+    },
+    {
+      source: surelymagazine
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-	<h1>Surely Magazine</h1>
-        <img src={surelymagazine} className="App-logo" alt="logo" />
-        <p>
-	  Website coming soon..
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/surely_magazine/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          instagram
-        </a>
-	<a
-          className="App-link"
-	  href="https://www.dropbox.com/s/m8fx4t0l5hac4xa/Paolo%20%40BGC.WAV"
-          target="_blank"
-          rel="noopener noreferrer"
-	>
-	  listen
-	</a>
-      </header>
+	  <Carousel views={images} />;
     </div>
   );
 }
